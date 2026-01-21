546ee004e8281de6edeb29fcb67970a0ae2cdad7
Rage Fade <ragef6@tutanota.de>
2026-01-20 20:01:34 -0500

    Installer update

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ eval "$(
 paru
 paru -S paru-bin
 #install all deps
-paru -S kitty starship nushell dotter-rs-bin cage hyprlock greetd-regreet zoxide graphicsmagick pqiv neovim paru swaync waybar wofi wlogout doas papirus-icon-theme
+paru -S kitty starship nushell dotter-rs-bin cage swaylock greetd-regreet zoxide pqiv neovim paru mako waybar wofi wlogout papirus-icon-theme  bibata-cursor-theme
 gsettings set org.gnome.desktop.interface icon-theme "Papirus"
 cargo install coreutils
 hyprpm add https://github.com/hyprwm/hyprland-plugins
