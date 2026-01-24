48acef030c8553ad2be9f04a2e76c7e51163ac4f
Rage Fade <ragef6@tutanota.de>
2026-01-24 10:28:39 -0500

    New, more useful swaylock system with wallpaper changes embedded

--- scripts/fullwal.sh
+++ scripts/fullwal.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 wallust run "$1" 
 killall -SIGUSR2 waybar
+cd $(dirname $(realpath "$HOME/.config/niri/config.kdl"))
+cd $(git rev-parse --show-toplevel)
+base=$(basename "$1")
+rm ./utils/swaylock
+sed "s/bg\.png/$base/g" ./utils/swaylock.temp > ./utils/swaylock
