cb4f94bf9488610a68ce8fd28681ad4eb2f2a9ca
Rage Fade <ragef6@tutanota.de>
2026-02-03 20:23:04 -0500

    Ignore bad firefox profiles

--- scripts/fullwal.sh
+++ scripts/fullwal.sh
@@ -7,4 +7,5 @@ base=$(basename "$1")
 rm ./utils/swaylock
 sed "s/bg\.png/$base/g" ./utils/swaylock.temp > ./utils/swaylock
 mv "$1" "$HOME/.mozilla/firefox/$(awk -F= '/^\[Profile/ {f=1} f==1 && $1~/^Path/ {print $2; 
-exit}' ~/.mozilla/firefox/profiles.ini)/chrome/wallpaper.png"
+exit}' ~/.mozilla/firefox/profiles.ini)/chrome/wallpaper.png" || echo ""
+
