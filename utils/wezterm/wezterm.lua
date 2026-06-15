5d3fe590c0dd7a1e988ede1e2b5480867413ecc7
Rage Fade <ragef6@tutanota.de>
2026-06-15 15:00:05 -0400

    Wezterm added in case

---
+++ utils/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new Bindoj:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 13
+config.color_scheme = 'wallust'
+config.window_background_opacity = {{opacity}}
+config.enable_tab_bar = false
+
+config.font = wezterm.font '{{font}}'
+-- config.kde_window_background_blur = true
+-- Finally, return the configuration to wezterm:
+return config
