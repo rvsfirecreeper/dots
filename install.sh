3a3d0223779bde0644231b079dfc0f5be03fee04
Rage Fade <ragef6@tutanota.de>
2026-06-27 13:39:27 -0400

    HAHA

--- install.sh
+++ install.sh
@@ -3,18 +3,14 @@ set -euo pipefail
 # Just making sure paru is installed
 cd "$HOME" || exit
 doas pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-eval "$(
-  cd paru || exit
-  makepkg -si
-)"
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin || exit
+makepkg -si
 # Updating the system because it needs to be updated
-paru
-paru -S paru-bin
 #install all deps
-paru -S kitty wallust mate-polkit starship nushell \
+yay -Syu kitty wallust mate-polkit starship nushell \
 dotter-rs-bin cage ttf-jetbrains-mono-nerd swayidle greetd-regreet zoxide pqiv \
-neovim paru mako waybar wofi wlogout wget \
+neovim mako waybar wofi wlogout wget \
 papirus-icon-theme  bibata-cursor-theme quickshell
 gsettings set org.gnome.desktop.interface icon-theme "Papirus"
 cargo install coreutils
@@ -23,7 +19,7 @@ cd dots || exit
 chsh -s /bin/nu
 touch /usr/share/backgrounds/bg
 doas chown $USER:$USER /usr/share/backgrounds/bg
-doas chmod 744 /usr/share/backgrounds/bg
+doas chmod 644 /usr/share/backgrounds/bg
 dotter deploy
 echo "Deployed. Running waypaper to start you off."
 echo "Done!"
