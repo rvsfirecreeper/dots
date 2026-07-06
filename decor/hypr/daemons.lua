0c346b7751940bdc838b041d7a03c219fb7eb1fc
Rage Fade <ragef6@tutanota.de>
2026-07-06 17:27:10 -0400

    Fix daemon start

--- decor/hypr/daemons.lua
+++ decor/hypr/daemons.lua
@@ -8,7 +8,6 @@
 
 local daemons = {}
 function daemons.start()
-    hl.exec_cmd(terminal)
     hl.exec_cmd("awww-daemon")
     hl.exec_cmd("swayidle before-sleep 'qs -p ~/.config/quickshell/Lock.qml'")
     hl.exec_cmd("quickshell -p ~/.config/quickshell/Bar.qml")
