545d3fa2c5384c9d792b45985aee3ff941abc3f0
Rage Fade <ragef6@tutanota.de>
2026-07-12 23:47:49 -0400

    Remove random bloat from fullwal

--- scripts/fullwal.sh
+++ scripts/fullwal.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 wallust run "$1" 
-cd $(dirname $(realpath "$HOME/.config/niri/config.kdl"))
-cd $(git rev-parse --show-toplevel)
 touch "$HOME/.rajlab-dotfiles/bg"
 cp "$HOME/$1" "$HOME/.rajlab-dotfiles/bg"
 touch "$HOME/.config/quickshell/bg"
 cp "$HOME/$1" "$HOME/.config/quickshell/bg"
-base=$(basename "$1")
