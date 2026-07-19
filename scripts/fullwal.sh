7e13b7b8a6cab0a0e375ebf9bdece3f4add84a84
Raj Singh <ragef6@tutanota.de>
2026-07-19 00:41:19 -0400

    Simply made the animation smoother

--- scripts/fullwal.sh
+++ scripts/fullwal.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-awww img -t grow "$1"
+awww img -t grow --transition-fps 60 "$1"
 wallust run "$1" 
 touch "$HOME/.rajlab-dotfiles/bg"
 cp "$HOME/$1" "$HOME/.rajlab-dotfiles/bg"
